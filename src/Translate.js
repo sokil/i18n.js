a8e0c38ab46624413449b49b8c5fe338e469afeb
Sokil <dmytro.sokil@gmail.com>
2017-06-06 22:53:59 +0300

    fix context

--- src/Translate.js
+++ src/Translate.js
@@ -1,90 +1,80 @@
-(function() {
-    /**
-     * Factory method
-     * @returns {Translate}
-     */
-    var Factory = function() {
+(function(context, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define('Translate', function () {
+            return (context['Translate'] = factory());
+        });
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        context['Translate'] = factory();
+    }
+})(this, function() {
+    // constructor
+    var Translate = function(messages) {
+        this.messages = messages || {};
+    }
 
-        var Translate = function(messages) {
-            this.messages = messages || {};
-        }
+    // class definition
+    Translate.prototype = {
 
-        Translate.prototype = {
+        /**
+         * List of messages
+         */
+        messages: {},
 
-            /**
-             * List of messages
-             */
-            messages: {},
+        /**
+         * Reset message list
+         */
+        reset: function() {
+            this.messages = {};
+            return this;
+        },
 
-            /**
-             * Reset message list
-             */
-            reset: function() {
-                this.messages = {};
-                return this;
-            },
+        /**
+         * Add new message
+         * @param {string} key
+         * @param {string} value
+         */
+        setMessage: function(key, value) {
+            this.messages[key] = value;
+            return this;
+        },
 
-            /**
-             * Add new message
-             * @param {string} key
-             * @param {string} value
-             */
-            setMessage: function(key, value) {
-                this.messages[key] = value;
-                return this;
-            },
-
-            /**
-             * Append list of messages
-             * @param messages
-             */
-            addMessages: function(messages) {
-                for(var key in messages) {
-                    this.messages[key] = messages[key];
-                }
-                return this;
-            },
-
-            /**
-             * Get message by key
-             *
-             * @param {string} key
-             * @param {string} placeholders
-             * @returns {string}
-             */
-            getMessage: function(key, placeholders) {
-                // get message
-                var message = this.messages[key] || key;
-
-                // replace placeholders
-                if (placeholders) {
-                    for(var placeholder in placeholders) {
-                        message = message.replace(
-                            new RegExp('{' + placeholder + '}', 'g'),
-                            placeholders[placeholder]
-                        );
-                    };
-                }
-
-                return message;
+        /**
+         * Append list of messages
+         * @param messages
+         */
+        addMessages: function(messages) {
+            for(var key in messages) {
+                this.messages[key] = messages[key];
             }
-        };
+            return this;
+        },
 
-        return Translate;
+        /**
+         * Get message by key
+         *
+         * @param {string} key
+         * @param {string} placeholders
+         * @returns {string}
+         */
+        getMessage: function(key, placeholders) {
+            // get message
+            var message = this.messages[key] || key;
+
+            // replace placeholders
+            if (placeholders) {
+                for(var placeholder in placeholders) {
+                    message = message.replace(
+                        new RegExp('{' + placeholder + '}', 'g'),
+                        placeholders[placeholder]
+                    );
+                };
+            }
+
+            return message;
+        }
     };
 
-    /**
-     * Module loading
-     */
-    (function(context, factory) {
-        if (typeof define === 'function' && define.amd) {
-            define('Translate', function () {
-                return (context['Translate'] = factory());
-            });
-        } else if (typeof exports === 'object') {
-            module.exports = factory();
-        } else {
-            context['Translate'] = factory();
-        }
-    })(this, Factory);
-})();
+    return Translate;
+});
